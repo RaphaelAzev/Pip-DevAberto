d3ced9f7bd6f926b18c1864856a0b3c3b18c3fab
raphaelazev <raphaelazev@hotmail.com>
2021-10-14 21:13:44 -0300

    Traducao para ingles do pacote feita

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 from dev_aberto import hello
+import babel.dates
+import gettext
+import datetime
+
+gettext.install('cli',localedir='locale')
 
 if __name__ == '__main__':
     date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    date_translate = babel.dates.format_datetime(datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z'))
+
+    print(_('Último commit feito em:'), date_translate, _(' por'), name)
